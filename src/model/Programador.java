657173b1ff10833e405d1628d7dd998ac1348caa
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2021-11-12 16:07:36 -0500

    Working

---
+++ src/model/Programador.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Programador {
+	private String nombre;
+	//private String telefono;
+	//private String direccion;
+	//private String eMail;
+	
+	public Programador(String nombre){
+		this.nombre = nombre;
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+
+	
+}
